e81c436da06efab4eea6f770c0f08e7ed5e07fa7
Xavier Chopin <bonjour@xavierchop.in>
2017-05-02 16:45:03 +0200

    First commit

---
+++ _settings/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ProductTranslator
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/assets/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/assets/Scripts/jquery.validate*"));
+
+            // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/assets/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/assets/Scripts/bootstrap.js",
+                      "~/assets/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/assets/Content/css").Include(
+                      "~/assets/Content/bootstrap.css",
+                      "~/assets/Content/site.css"));
+        }
+    }
+}
