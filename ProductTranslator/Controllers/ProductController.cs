0a1ea7fe99184220a06a410813ea15e2c03b8e84
Xavier Chopin <bonjour@xavierchop.in>
2017-05-03 17:00:44 +0200

    Added new Controller

---
+++ ProductTranslator/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Xml;
+
+namespace ProductTranslator.Controllers
+{
+    public class ProductController : BaseController
+    {
+        public ActionResult Index(String id)
+        {
+      
+            if (id != null && System.IO.File.Exists(Server.MapPath("~/Resources/DB/Eau/fr/" + id + ".xml")))
+            {
+                this.Flash("success", "File exists !");
+                return RedirectToAction("Index", "Home");
+            }
+
+            this.Flash("danger", "Error: this product id does not exist!");
+            return RedirectToAction("Index", "Home");
+        }
+
+        private void AllElement(XmlNode node, String element)
+        {
+            if (node.HasChildNodes)
+            {
+                foreach (XmlNode subNode in node.ChildNodes)
+                {
+                    Console.WriteLine(subNode.Name);
+                }
+            }
+        }
+
+    }
+}
