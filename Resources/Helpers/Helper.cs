5786e9ee7b8cafee0e018d6bffd4a6a3bfcf4dc1
Xavier Chopin <bonjour@xavierchop.in>
2017-05-03 11:45:22 +0200

    Added Market

---
+++ Resources/Helpers/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductTranslator.Resources.Helpers
+{
+    public class Helper : Controller
+    {
+       
+        public static void flash(String type, String message)
+        {
+            ViewBag.flash_message = message;
+            ViewBag.flash_type = type;
+        }
+
+        public static ActionResult render(String file)
+        {
+            return View(file + ".cshtml");
+        }
+
+
+    }
+}
