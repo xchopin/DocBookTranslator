9728ccb3dc7f14d5b78e1691a2934dc978cc5fe1
Xavier Chopin <bonjour@xavierchop.in>
2017-05-03 11:45:47 +0200

    Added flash messages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Xml;
+using ProductTranslator.Resources.Helpers;
 
 namespace ProductTranslator.Controllers
 {
     public class HomeController : Controller
     {
+
+    
         public ActionResult Index()
         {
-
             XmlDocument xml = new XmlDocument();
             xml.Load(Server.MapPath("~/Resources/markets.xml"));
-            XmlNodeList xnList = xml.SelectNodes("/markets/market");
-            ViewBag.markets = xnList;
+            ViewBag.markets = xml.SelectNodes("/markets/market");
+
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult SearchProduct()
         {
-            ViewBag.Message = "Your application description page.";
+            //String productId = Request.Form["productId"];
 
-            return View();
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load(Server.MapPath("~/Resources/markets.xml"));
+            ViewBag.markets = xml.SelectNodes("/markets/market");
+
+            Helper.flash("danger", "This product does not exist!");
+          
+            return Helper.render("Home/Index");
+        
+ 
         }
 
         public ActionResult Contact()
