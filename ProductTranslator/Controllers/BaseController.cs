f87d6729201fec9e39f4398b14c5700d1f633bff
Xavier Chopin <bonjour@xavierchop.in>
2017-05-03 15:53:49 +0200

    Added default controller

---
+++ ProductTranslator/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductTranslator.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected void Flash(String type, String message)
+        {
+            TempData["flash.type"] = type;
+            TempData["flash.message"] = message;
+        }
+
+        protected ActionResult Render(String file)
+        {
+            return View("~/Views/" + file + ".cshtml");
+        }
+    }
+}
