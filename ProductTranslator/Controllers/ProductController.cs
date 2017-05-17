e12bc1247626aebf949d6814b5e9e5a4217596a5
Xavier Chopin <bonjour@xavierchop.in>
2017-05-17 11:00:01 +0200

    Changed loop checking

--- ProductTranslator/Controllers/ProductController.cs
+++ ProductTranslator/Controllers/ProductController.cs
@@ -27,6 +27,7 @@ namespace ProductTranslator.Controllers
            {
                 XmlDocument xml = new XmlDocument();
                 data = new List<String>();
+                xml.PreserveWhitespace = true;
                 xml.Load(TempData["file"].ToString());
                 AllElement(xml, "title");
                 AllElement(xml, "para");
@@ -36,8 +37,7 @@ namespace ProductTranslator.Controllers
                 ViewBag.forms = data;
                 ViewBag.productId = id;
 
-                xml.Load(Server.MapPath("~/Resources/languages.xml"));
-                ViewBag.languages = xml.SelectNodes("/languages/language");
+                this.sendLanguages();
 
                 TempData["path"] = TempData["path"]; // To be sure it persists: TempData has a short-lived instance
                 TempData["file"] = TempData["file"];
@@ -50,10 +50,78 @@ namespace ProductTranslator.Controllers
         }
 
 
-       /**
-        * Create a XML file of the translation previously done
-        * 
-        */
+        /*
+         * Create the form page for editing a datasheet
+         * 
+         */ 
+        public ActionResult EditForm(String id)
+        {
+             if (TempData["file"] != null && TempData["path"] != null)
+           {
+
+                XmlDocument xml = new XmlDocument();
+                data = new List<String>();
+                xml.PreserveWhitespace = true;
+
+                // Original Content [FR]
+                String path = TempData["path"].ToString();
+                xml.Load(path.Remove(path.Length - 3) + "fr\\" + id + ".xml");
+                AllElement(xml, "title");
+                AllElement(xml, "para");
+                AllElement(xml, "emphasis");
+                AllElement(xml, "entry");
+
+                ViewBag.forms = data;
+                ViewBag.productId = id;
+                ViewBag.languageId = TempData["languageId"];
+
+                // Edited Content
+                data = new List<String>();
+                xml.PreserveWhitespace = true;
+                xml.Load(TempData["file"].ToString());
+                AllElement(xml, "title");
+                AllElement(xml, "para");
+                AllElement(xml, "emphasis");
+                AllElement(xml, "entry");
+
+                if (data.Count != ViewBag.forms.Count)
+                {
+                    this.Flash("warning", "This translation was not well settled. Create one instead");
+                    return RedirectToAction("Index", "Home");
+                }
+
+                // Load the name of the language and check if it exists 
+                try
+                {
+                    xml.Load(Server.MapPath("~/Resources/languages.xml"));
+                    XmlNode node = xml.SelectSingleNode("/languages/language[@id='" + TempData["languageId"] + "']");
+                    ViewBag.language = node.Attributes["name"].Value;
+                }
+                catch (Exception e)
+                {
+                    this.Flash("danger", "This language does not exist, please verify the xml language file (developer)");
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ViewBag.edited = data;
+
+                ViewBag.productId = id;
+
+                // To be sure it persists: TempData has a short-lived instance
+                TempData["path"] = TempData["path"];
+                TempData["file"] = TempData["file"];
+
+                return View();
+           }
+
+            this.Flash("danger", "Please use the quicksearch instead of typing in the address bar");
+            return RedirectToAction("Index", "Home");
+        }
+
+        /**
+         * Create a XML file of the translation previously done
+         * 
+         */
         public ActionResult TranslateProduct()
         {
 
@@ -61,53 +129,52 @@ namespace ProductTranslator.Controllers
             {
                 String id = Request.Params["productId"];
                 String languageId = Request.Params["input_languageId"];
-
                 XmlDocument xml = new XmlDocument();
-                xml.Load(Server.MapPath("~/Resources/languages.xml"));
-
                 // Prevents against bad path ! (Check if the language exists)
                 try
                 {
+                    xml.Load(Server.MapPath("~/Resources/languages.xml"));
                     String test = xml.SelectSingleNode("/languages/language[@id='" + languageId + "']").InnerText;
-
-                    // Delete the two last letters of the source dir (in this case 'fr')
-                    String marketPath = TempData["path"].ToString().Remove(TempData["path"].ToString().Length - 3);
-                    String dirName = marketPath + languageId + "/";
-                   
-                    // Works only if the directory does not exist (https://msdn.microsoft.com/en-us/library/54a0at6s.aspx)
-                    Directory.CreateDirectory(dirName);
-
-                    // Node values start at i = 2 in POST params (if you don't know why check the view)
-                    int fieldsNumber = Request.Form.Count - 2;  
-                    data = new List<String>(fieldsNumber);
-                    for (int i = 0; i < fieldsNumber; i++)
-                    {
-                        data.Add(Request.Form["translation" + i]);
-                    }
-
-                    // This part change and save the new content
-                    xml.Load(TempData["file"].ToString());
-                    ReplaceContent(xml, "title");
-                    ReplaceContent(xml, "para");
-                    ReplaceContent(xml, "emphasis");
-                    ReplaceContent(xml, "entry");
-                    xml.PreserveWhitespace = true;
-                    xml.Save(dirName + id + ".xml");
-
-                    this.Flash("success", "Translation succeeded!");
-                    return RedirectToAction("Index", "Home");
                 }
                 catch (Exception e)
                 {
-                    Debug.WriteLine(e);
+                    //Debug.WriteLine(e);
                     this.Flash("danger", "This language does not exist, please verify the xml language file (developer)");
                     return RedirectToAction("Index", "Home");
                 }
+
+                // Delete the two last letters of the source dir (in this case 'fr')
+                String marketPath = TempData["path"].ToString().Remove(TempData["path"].ToString().Length - 3);
+                String dirName = marketPath + languageId + "/";
+
+                // Works only if the directory does not exist (https://msdn.microsoft.com/en-us/library/54a0at6s.aspx)
+                Directory.CreateDirectory(dirName);
+
+                data = new List<String>();
+
+                foreach (String key in Request.Form)
+                {
+                    if (key.StartsWith("translation"))
+                        data.Add(Request.Form[key]);
+                }
+
+                // This part change and save the new content
+                xml.PreserveWhitespace = true;
+                xml.Load(TempData["file"].ToString());
+                ReplaceContent(xml, "title");
+                ReplaceContent(xml, "para");
+                ReplaceContent(xml, "emphasis");
+                ReplaceContent(xml, "entry");
+                xml.Save(dirName + id + ".xml");
+
+                this.Flash("success", "Translation succeeded!");
+                return RedirectToAction("Index", "Home");
             }
 
             this.Flash("danger", "An error occured, you might be faster to fill the fields.");
             return RedirectToAction("Index", "Home");
         }
+
 
 
         /**
@@ -124,16 +191,20 @@ namespace ProductTranslator.Controllers
                 {
                     if (subNode.Name == element)
                     {
-                        String value = subNode.InnerText;
-                        if (!Regex.IsMatch(value, @"^\d+$") && !value.StartsWith("[!!!!!") && value.Length > 1)
-                            data.Add(subNode.InnerText);
+                        if (!subNode.InnerXml.TrimStart().StartsWith("<")) // Solve the issue of twice elements
+                        {
+                            String value = subNode.InnerText;
+                            if (!Regex.IsMatch(value, @"^\d+$") && !value.StartsWith("[!!!!!") && !String.IsNullOrWhiteSpace(value))
+                                data.Add(subNode.InnerText);
+                        }
+                       
                     }
                     AllElement(subNode, element);
                 }
             }
         }
 
-
+        
         /**
          * Replace the content of all the elements, given, in the XML file loaded
          * 
@@ -148,15 +219,17 @@ namespace ProductTranslator.Controllers
                 {
                     if (subNode.Name == element)
                     {
-                        String value = subNode.InnerText;
-                        if (!Regex.IsMatch(value, @"^\d+$") && !value.StartsWith("[!!!!!") && value.Length > 1)
+                        if (!subNode.InnerXml.TrimStart().StartsWith("<"))
                         {
-                            subNode.InnerText = data[0];
-                            data.RemoveAt(0);
+                            String value = subNode.InnerText;
+                            if (!Regex.IsMatch(value, @"^\d+$") && !value.StartsWith("[!!!!!") && !String.IsNullOrWhiteSpace(value))
+                            {
+
+                                subNode.InnerText = data[0];
+                                data.RemoveAt(0);
+                            }
                         }
-
                     }
-
                     this.ReplaceContent(subNode, element);
                 }
             }
