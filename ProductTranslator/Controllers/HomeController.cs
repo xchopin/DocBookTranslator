ebaad2d6cb9b9af28ded56e224bd40901626a26f
Xavier Chopin <bonjour@xavierchop.in>
2017-05-17 09:13:09 +0200

    Added edit form

--- ProductTranslator/Controllers/HomeController.cs
+++ ProductTranslator/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace ProductTranslator.Controllers
     public class HomeController : BaseController
     {
 
-        /**
+        /*
          * Render the index page
          * 
          */
@@ -22,12 +22,14 @@ namespace ProductTranslator.Controllers
             return View();
         }
 
+
         /*
          * Find a product in the database and redirect to the form page
          * 
-         */ 
+         */
         public ActionResult SearchProduct()
         {
+            
             String productId = Request["productId"];
 
             String[] directories = Directory.GetDirectories(Server.MapPath("~/Resources/DB/"), "*", System.IO.SearchOption.AllDirectories);
@@ -37,6 +39,62 @@ namespace ProductTranslator.Controllers
             foreach (String directory in directories)
             {
                 if (directory.EndsWith("fr"))
+                {
+                    if (System.IO.File.Exists(directory + "\\" + productId + ".xml"))
+                    {
+                        TempData["path"] = directory + "\\";
+                        TempData["file"] = directory + "\\" + productId + ".xml";
+                    }
+                }
+            }
+
+
+            if (TempData["path"] == null)
+            {
+                this.Flash("danger", "Error: this product id does not exist!");
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "Product", new { id = productId });
+        }
+
+        public ActionResult SearchEdit()
+        {
+            this.sendLanguages();
+            return View();
+        }
+
+
+       /*
+        * Find a product in the database and redirect to the form page
+        * 
+        */ 
+        public ActionResult SearchTranslatedProduct()
+        {
+            String productId = Request["productId"];
+            String languageId = Request["input_languageId"];
+
+            // Prevents against bad path ! (Check if the language exists)
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.Load(Server.MapPath("Resources/languages.xml"));
+                String test = xml.SelectSingleNode("/languages/language[@id='" + languageId + "']").InnerText;
+            }
+            catch (Exception e)
+            {
+                this.Flash("danger", "This language does not exist, please verify the xml language file (developer)");
+                return RedirectToAction("Index", "Home");
+            }
+        
+            String[] directories = Directory.GetDirectories(Server.MapPath("~/Resources/DB/"), "*", System.IO.SearchOption.AllDirectories);
+
+            TempData["path"] = null;
+            TempData["languageId"] = languageId;
+
+            foreach (String directory in directories)
+            {
+                if (directory.EndsWith(languageId))
                 {
                     if (System.IO.File.Exists(directory + "\\" +  productId + ".xml"))
                     {
@@ -53,7 +111,8 @@ namespace ProductTranslator.Controllers
                 return RedirectToAction("Index");
             }
 
-            return RedirectToAction("Index", "Product", new { id = productId });
+
+            return RedirectToAction("EditForm", "Product", new { id = productId });
         }
     }
 }
